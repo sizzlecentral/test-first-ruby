31298e28863508bcc4f33c7e3f81266c873c59eb
Don Cooper <don_cooper@hotmail.com>
2017-06-05 16:22:15 -0400

    02 clear

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,13 @@
+def add(num1, num2)
+  @total = num1 + num2
+  return @total
+end
+
+def subtract(num1, num2)
+  @total = num1 - num2
+end
+
+def sum(array)
+  @total = array.sum
+  return @total
+end
