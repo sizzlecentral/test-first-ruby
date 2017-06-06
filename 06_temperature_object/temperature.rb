52aff3b176fe1f03d5fa40b18182df76c6379698
Don Cooper <don_cooper@hotmail.com>
2017-06-06 19:03:37 -0400

    Set up sub classes, not working yet

--- 06_temperature_object/temperature.rb
+++ 06_temperature_object/temperature.rb
@@ -1,18 +1,40 @@
 class Temperature
 
   def initialize(temp)
-    @temperature = temp
+    @in_fahrenheit = temp[:f]
+    @in_celsius = temp[:c]
   end
 
-
-  def to_fahrenheit(temp)
+  def to_fahrenheit
     @fahrenheit = ((temp / (5.0/9)) + 32)
     return @fahrenheit
   end
 
-  def to_celsius(temp)
+  def to_celsius
     @celcius = (temp - 32) * (5.0/9)
     return @celcius
+  end
+
+end
+
+
+# ----- Celcius ----- #
+
+class Celsius < Temperature
+
+  def initialize(temp)
+
+  end
+
+end
+
+
+# ----- Fahrenheit ----- #
+
+class Fahrenheit < Temperature
+
+  def initialize(temp)
+
   end
 
 end
